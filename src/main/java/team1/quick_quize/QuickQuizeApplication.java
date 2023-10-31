8c50495218c5b4dce8f56f6c5fa2c8c34572123b
murakou1 <e1b21105@oit.ac.jp>
2023-10-31 15:48:42 +0900

    リポジトリの初期化

---
+++ src/main/java/team1/quick_quize/QuickQuizeApplication.java
@@ -0,0 +1,13 @@
+package team1.quick_quize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickQuizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickQuizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team1/quick_quize/QuickQuizeApplicationTests.java
@@ -0,0 +1,13 @@
+package team1.quick_quize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickQuizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
