8579bdfe1b863e79f3e77623bbeb574edc5f12ae
murakou1 <e1b21105@oit.ac.jp>
2023-11-07 18:54:59 +0900

    DBのtableの更新と問題文のinsert

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
-INSERT INTO quiz (no,content,answer) VALUES (1,'トランザクションが，データベースに対する更新処理を完全に行うか，全く処理しなかったのように取り消すか，のどちらかの結果になることを保証する特性はどれか。','原子性');
-INSERT INTO userinfo (userName,age,height) VALUES ('いがき',30,176.1);
+INSERT INTO quiz (content,choice1,choice2,choice3,choice4,answer) VALUES ('フリップフロップ回路を利用した高速なメモリはどれか？','DRAM','RDRAM','SDRAM','SRAM',4);
+INSERT INTO quiz (content,choice1,choice2,choice3,choice4,answer) VALUES ('50MIPSのプロセッサの平均命令実行時間は幾らか？','20ナノ秒','50ナノ秒','2マイクロ秒','5マイクロ秒',1);
+INSERT INTO quiz (content,choice1,choice2,choice3,choice4,answer) VALUES ('UMLの図のうち、業務要件定義において、業務フローを記述する際に使用する、処理の分岐や並行処理、処理の同期などを表現できる図はどれか？','アクティビティ図','クラス図','状態遷移図','ユースケース図',1);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,16 @@
-CREATE TABLE userinfo (
-    userName VARCHAR NOT NULL PRIMARY KEY,
-    age INT,
-    height DOUBLE NOT NULL
+CREATE TABLE users (
+    id IDENTITY,
+    username VARCHAR NOT NULL,
+    point INT
 );
 
 CREATE TABLE quiz(
-    no INT,
+    no IDENTITY,
     content VARCHAR,
-    answer VARCHAR
+    choice1 VARCHAR,
+    choice2 VARCHAR,
+    choice3 VARCHAR,
+    choice4 VARCHAR,
+    answer INT
 );
+//content:問題文  choice:選択肢  answer:答えの番号
