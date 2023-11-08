62ceb01c45b1dd8ddedef596ed1cb3cf78a911f2
吉本颯汰 <e1b21116@oit.ac.jp>
2023-11-08 11:02:14 +0900

    クイズのinsert

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 INSERT INTO quiz (content,choice1,choice2,choice3,choice4,answer) VALUES ('フリップフロップ回路を利用した高速なメモリはどれか？','DRAM','RDRAM','SDRAM','SRAM',4);
 INSERT INTO quiz (content,choice1,choice2,choice3,choice4,answer) VALUES ('50MIPSのプロセッサの平均命令実行時間は幾らか？','20ナノ秒','50ナノ秒','2マイクロ秒','5マイクロ秒',1);
 INSERT INTO quiz (content,choice1,choice2,choice3,choice4,answer) VALUES ('UMLの図のうち、業務要件定義において、業務フローを記述する際に使用する、処理の分岐や並行処理、処理の同期などを表現できる図はどれか？','アクティビティ図','クラス図','状態遷移図','ユースケース図',1);
+INSERT INTO quiz (content,choice1,choice2,choice3,choice4,answer) VALUES ('トランザクションが，データベースに対する更新処理を完全に行うか，全く処理しなかったのように取り消すか，のどちらかの結果になることを保証する特性はどれか？','一貫性(consistency)','原子性(atomicity)','耐久性(durability)','独立性(isolation)',2);
+INSERT INTO quiz (content,choice1,choice2,choice3,choice4,answer) VALUES ('アジャイル開発手法のスクラムにおいて，開発チームの全員が1人ずつ"昨日やったこと"，"今日やること"，"障害になっていること"などを話し，全員でプロジェクトの状況を共有するイベントはどれか？','スプリントプランニング','スプリントレビュー','デイリースクラム','レトロスペクティブ',3);
+INSERT INTO quiz (content,choice1,choice2,choice3,choice4,answer) VALUES ('IPv4ネットワークにおいて，ネットワークの疎通確認に使われるものはどれか？','BOOTP','DHCP','MIB','ping',4);
