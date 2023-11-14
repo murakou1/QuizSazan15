c598571dc09901041927b8cd911a5fa477d5c726
murakou1 <e1b21105@oit.ac.jp>
2023-11-14 16:03:21 +0900

    home.htmlの更新とwait.htmlへの画面遷移

--- src/main/java/team1/quick_quize/controller/QuuizeController.java
+++ src/main/java/team1/quick_quize/controller/QuuizeController.java
@@ -1,5 +1,7 @@
 package team1.quick_quize.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +13,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class QuuizeController {
 
   @GetMapping("/home")
-  public String home() {
+  public String home(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    model.addAttribute("loginUser", loginUser);
     return "home.html";
+  }
+
+  @GetMapping("/wait")
+  public String wait(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    model.addAttribute("loginUser", loginUser);
+    return "wait.html";
   }
 
 }
