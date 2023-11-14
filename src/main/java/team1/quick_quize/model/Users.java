83fc223427489bbe646077c9e1a93031690b99ed
e1b21101 <e1b21101@st.oit.ac.jp>
2023-11-14 16:36:49 +0900

    wait.htmlの更新

---
+++ src/main/java/team1/quick_quize/model/Users.java
@@ -0,0 +1,34 @@
+package team1.quick_quize.model;
+
+public class Users {
+  int id;
+  String userName;
+  int point;
+
+  // Thymeleafでフィールドを扱うためにはgetter/setterが必ず必要
+  // vscodeのソースコード右クリック->ソースアクションでsetter/getterを簡単に追加できる
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public int getpoint() {
+    return point;
+  }
+
+  public void setChamberName(int point) {
+    this.point = point;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+}
---
+++ src/main/java/team1/quick_quize/model/UsersMapper.java
@@ -0,0 +1,38 @@
+package team1.quick_quize.model;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UsersMapper {
+
+  @Select("SELECT id,userName,point from Users where id = #{id}")
+  Users selectById(int id);
+
+  /**
+   * #{userName}などはinsertの引数にあるChamberクラスのフィールドを表しています 引数に直接String
+   * userNameなどと書いてもいけるはず
+   * 下記のOptionsを指定すると，insert実行時にAuto incrementされたIDの情報を取得できるようになる useGeneratedKeys
+   * = true -> Keyは自動生成されることを表す keyColumn : keyになるテーブルのカラム名 keyProperty :
+   * keyになるJavaクラスのフィールド名
+   *
+   * @param Users
+   */
+  @Insert("INSERT INTO Users (userName,point) VALUES (#{userName},#{point});")
+  @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
+  void insertChamber(Users Users);
+
+  @Select("SELECT * from chamber where chamberName = #{chamberName}")
+  ArrayList<Users> selectAllByChamberName(int point);
+
+  /**
+   * DBのカラム名とjavaクラスのフィールド名が同じ場合はそのまま代入してくれる（大文字小文字の違いは無視される）
+   * カラム名とフィールド名が異なる場合の対応も可能だが，いきなり複雑になるので，selectで指定するテーブル中のカラム名とクラスのフィールド名は同一になるよう設計することが望ましい
+   *
+   * @return
+   */
+}
