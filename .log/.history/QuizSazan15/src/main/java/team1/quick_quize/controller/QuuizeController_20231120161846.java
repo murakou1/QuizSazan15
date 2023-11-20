88e3abaaf2bd1e4f3e5f3e79add8bf07853f975b
e1b21046 <e1b21046@oit.ac.jp>
2023-11-20 16:22:50 +0900

    home.htmlのレイアウト変更

---
+++ .log/.history/QuizSazan15/src/main/java/team1/quick_quize/controller/QuuizeController_20231120161846.java
@@ -0,0 +1,41 @@
+package team1.quick_quize.controller;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import team1.quick_quize.model.Users;
+import team1.quick_quize.model.UsersMapper;
+
+@Controller
+public class QuuizeController {
+
+  @Autowired
+  UsersMapper usersMapper;
+
+  @PostMapping("/home")
+  public String home(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    model.addAttribute("loginUser", loginUser);
+    return "home.html";
+  }
+
+  @GetMapping("/wait")
+  public String wait(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    Users User = new Users();
+    User.setUserName(loginUser);
+    User.setPoint(0);
+
+    usersMapper.insertUser(User);
+    model.addAttribute("loginUser", loginUser);
+    return "wait.html";
+  }
+
+}
